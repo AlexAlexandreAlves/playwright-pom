7d7afddf71b617f2cab468ef39cbc0050ee023b3
AlexAlexandreAlves <alexalexandrealves@gmail.com>
2024-01-31 15:53:25 -0300

    initial commit :rocket:

---
+++ setup/global-setup.ts
@@ -0,0 +1,24 @@
+import { chromium, FullConfig } from "@playwright/test";
+
+
+async function globalSetup(config: FullConfig) {
+    const { baseURL, storageState } = config.projects[0].use;
+    const browser = await chromium.launch({ headless: true, timeout: 10000 });
+    const context = await browser.newContext(); 
+    const page = await browser.newPage();
+
+    try {
+        await context.tracing.start({ screenshots: true });
+        await page.goto(baseURL!);
+        await page.context().storageState({ path: storageState as string });
+        await context.tracing.stop({ path: './test-results/setup-trace.zip' });
+        await browser.close();
+
+    } catch (error) {
+        await context.tracing.stop({ path: './test-results/setup-trace.zip' });
+        await browser.close();
+        throw error;
+    }
+}
+
+export default globalSetup;
